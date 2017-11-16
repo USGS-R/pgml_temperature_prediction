6017a292ebb15f78291723f05330fc3ebe4ff0dd
wdwatkins <wwatkins@usgs.gov>
2017-11-16 12:48:20 -0600

    almost have duplicates removed

--- R/bind_wqp_uw.R
+++ R/bind_wqp_uw.R
@@ -9,6 +9,6 @@ wqp <- wqp %>% mutate(buoy = FALSE, year4 = year(Date),
                       month = month(Date), daynum = yday(Date)) %>% 
       rename(DateTime=Date)
 uw <- uw %>% rename(DateTime=sampledate)
-all_data <- bind_rows(wqp, uw)
+all_data <- bind_rows(wqp, uw) %>% rename(Depth=depth, temp = wtemp)
 write.table(x = all_data, file = "obs/mendota/mendota_combined.tsv", sep = "\t",
             row.names = FALSE)
--- R/parse_uw_data.R
+++ R/parse_uw_data.R
@@ -5,16 +5,22 @@ buoy <- read.csv('obs/mendota/partial/mendota_daily_buoy.csv')
 #flag code definitions are in the EML format on the UW limno data site
 #https://lter.limnology.wisc.edu/data
 buoy <- buoy %>% filter(!flag_wtemp %in% c("A11N", "D", "H")) %>% 
-  mutate(sampledate = as.Date(sampledate), buoy = TRUE)
+  mutate(sampledate = as.Date(sampledate), buoy = TRUE) %>% filter(!is.na(wtemp))
 
 
 discrete <- read.csv('obs/mendota/partial/long_term_ntl.csv')
-
-discrete <- discrete %>% filter(lakeid == "ME") %>% 
+#deal with duplicates and remove NAs  
+discrete <- discrete %>% filter(lakeid == "ME" & !is.na(wtemp)) %>% 
   select(year4, daynum, sampledate, depth, wtemp, flagwtemp) %>% 
   mutate(sampledate = as.Date(sampledate), month = month(sampledate),
-         buoy = FALSE) %>% rename(flag_wtemp = flagwtemp)
+         buoy = FALSE) %>% rename(flag_wtemp = flagwtemp) %>% 
+  anti_join(buoy, by = c("sampledate", "depth")) #keep buoy data over hand 
 #NOTE: no flagged mendota data, but will want to check for other lakes
+
+#remove BOTH pairs of duplicated data
+date_depth <- select(discrete, sampledate, depth)
+dup_indices <- which(duplicated(date_depth) | duplicated(date_depth, fromLast = TRUE))
+discrete <- slice(discrete, -dup_indices)
 
 combined_df <- bind_rows(buoy, discrete)
 saveRDS(object = combined_df, file = 'obs/mendota/partial/mendota_uw.rds')
