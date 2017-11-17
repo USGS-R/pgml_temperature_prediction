0ec789de13147a47598658c1c12f71c5fe7679d2
wdwatkins <wwatkins@usgs.gov>
2017-11-17 11:35:01 -0600

    bring in longer mendota record

--- R/parse_uw_data.R
+++ R/parse_uw_data.R
@@ -1,3 +1,12 @@
+remove_duplicates <- function(df, colNames) {
+  #remove both pairs of duplicates from df based on colNames
+  df_cols <- select(df, colNames)
+  dup_indices <- which(duplicated(df_cols) | duplicated(df_cols, fromLast = TRUE))
+  df_nodups <- slice(df, -dup_indices)
+  return(df_nodups)
+}
+
+
 #assimilate the discrete and buoy data from UW
 library(dplyr)
 library(lubridate)
@@ -17,10 +26,23 @@ discrete <- discrete %>% filter(lakeid == "ME" & !is.na(wtemp)) %>%
   anti_join(buoy, by = c("sampledate", "depth")) #keep buoy data over hand 
 #NOTE: no flagged mendota data, but will want to check for other lakes
 
-#remove BOTH pairs of duplicated data
-date_depth <- select(discrete, sampledate, depth)
-dup_indices <- which(duplicated(date_depth) | duplicated(date_depth, fromLast = TRUE))
-discrete <- slice(discrete, -dup_indices)
+discrete <- remove_duplicates(discrete, c("sampledate", "depth"))
 
 combined_df <- bind_rows(buoy, discrete)
+
+#long-term from LTER data portal
+#there are characters in depth column, but before 1980
+long <- read.csv(file = 'obs/mendota/partial/Lake_Mendota_temps_long.csv',
+                 stringsAsFactors = FALSE, colClasses = c(sampledate="Date"))
+long <- filter(long, sampledate > "1980-01-01") %>% 
+  select(-day, -reps, -average, -observer, -ObsTime, -Loc) %>% 
+  rename(wtemp=watertemp) %>% mutate(daynum = yday(sampledate), 
+                                     buoy=FALSE, depth=as.numeric(depth)) %>% 
+  anti_join(combined_df, by = c("sampledate", "depth"))
+long_nodups <- remove_duplicates(long, c("sampledate", "depth"))
+
+combined_df <- bind_rows(combined_df, long_nodups)
+
+
+
 saveRDS(object = combined_df, file = 'obs/mendota/partial/mendota_uw.rds')
