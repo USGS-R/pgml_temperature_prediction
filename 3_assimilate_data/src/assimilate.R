54b51f30fca4abeb4a58fe97c5eaac0aab57361f
wdwatkins <wwatkins@usgs.gov>
2018-02-08 12:01:00 -0600

    data modeling needed and not or

--- 3_assimilate_data/src/assimilate.R
+++ 3_assimilate_data/src/assimilate.R
@@ -2,7 +2,7 @@
 clean_universal <- function(df, base_model_cfg, max_depth) {
   cfg <- read_yaml(base_model_cfg)
   df_clean <- df %>% filter(temp < cfg$max_temp & Depth < max_depth & 
-                       (DateTime > cfg$start_date | DateTime < cfg$end_date)) %>% 
+                       (DateTime > cfg$start_date & DateTime < cfg$end_date)) %>% 
     filter(!is.na(temp) & !is.na(Depth) & !is.na(DateTime))
   
 }
