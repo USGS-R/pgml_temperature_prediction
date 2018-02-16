7e62a0733da1000534ed715eff1d6d591ee9710b
wdwatkins <wwatkins@usgs.gov>
2018-02-16 12:22:02 -0600

    assimilate data working via remake

--- 3_assimilate_data/src/assimilate.R
+++ 3_assimilate_data/src/assimilate.R
@@ -11,22 +11,23 @@ clean_universal <- function(df, base_model_cfg, max_depth) {
 #filter out data for this site from files
 #then get WQP data
 #do universal cleaning + depth filtering based on lake nml max depth
-
-assimilate_data <- function(nhd_id, files, state_src, state_id) {
+#dots are cleaned files to look in for this site
+assimilate_data <- function(nhd_id, state_src, state_id, wqp_file, ...) {
+  cleaned_files <- lapply(c(...), sc_retrieve, remake_file = "1_data_s3_cleaned.yml")
+  wqp_file <- sc_retrieve(wqp_file, remake_file = "1_data_wqp.yml")
   site_data <- data.frame()
-  for(f in files) {
+  for(f in cleaned_files) {
     whole_cleaned_file <- readRDS(f)
     filtered <- filter_(whole_cleaned_file, paste(state_src, "%in% state_id"))
     site_data <- bind_rows(site_data, filtered)
   }
-  wqp_file_name <- paste(nhd_id, "rds", sep = ".")
-  wqp_data <- readRDS(file.path("1_data_wqp/out", wqp_file_name))
+  wqp_data <- readRDS(wqp_file)
   wqp_data <- rename(wqp_data, DateTime = Date, temp = wtemp, 
                      Depth = depth)
   #deal with duplicates; keep the non-WQP value
   site_data <- distinct(bind_rows(site_data, wqp_data), DateTime, Depth, 
                         .keep_all = TRUE)
-  #TODO: WTF distinct is missing some?  manually remove what anyDuplicated catches?
+  #TODO: distinct is missing some?  manually remove what anyDuplicated catches?
   #try a data.table function?
   
   while( anyDuplicated(site_data[c("DateTime", "Depth")]) != 0){
@@ -43,6 +44,6 @@ assimilate_data <- function(nhd_id, files, state_src, state_id) {
                              max_depth)
   outfile <- paste0("3_assimilate_data/out/", nhd_id, ".rds")
   saveRDS(object = cleaned, file = outfile)
-  s3_put(remote_ind = as_ind_file(outfile), local_source =  as_ind_file(outfile)) 
+  s3_put(remote_ind = as_ind_file(outfile), local_source =  outfile) 
 }
 
