6d83c9db7450c2eb321d93867749853b89fc696a
wdwatkins <wwatkins@usgs.gov>
2018-02-20 10:21:10 -0700

    eliminate redundant steps in 1_download and 1_s3

--- remake_templates/src/render_s3.R
+++ remake_templates/src/render_s3.R
@@ -1,6 +1,9 @@
 render_s3 <- function(){
   template <- readLines('remake_templates/s3.template')
-  targets <- read.csv('lib/crosswalks/rawfile_to_id_crosswalk.csv', stringsAsFactors = FALSE)$file
+  lakes <- read.csv('lib/crosswalks/rawfile_to_id_crosswalk.csv', 
+                    stringsAsFactors = FALSE) %>% 
+          filter(id != "UWID")
+  targets <- lakes$file
   
   targets <- list(targets=targets)
   out <- whisker.render(template = template, data = targets)
